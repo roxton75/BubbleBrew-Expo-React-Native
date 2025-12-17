578e514fd708bd1b26152fa04ab1ff5d19425f59
rudrx_75 <142597060+roxton75@users.noreply.github.com>
2025-12-17 19:43:07 +0530

    Implement splash screen functionality
    Add splash screen handling to RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 
@@ -12,7 +14,20 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const colorScheme = useColorScheme();
+
+  SplashScreen.preventAutoHideAsync();
+
+  useEffect(() => {
+    const prepare = async () => {
+      // wait for app setup (Realm, fonts, etc.)
+      await new Promise((resolve) => setTimeout(resolve, 300));
+      await SplashScreen.hideAsync();
+    };
+
+    prepare();
+  }, []);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
