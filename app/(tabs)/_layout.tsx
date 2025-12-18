b399fa5a2dfed4c0653aed6ac55b96b268b8448c
rudrx_75 <142597060+roxton75@users.noreply.github.com>
2025-12-19 01:31:51 +0530

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,43 @@
-// app/(tabs)/_layout.tsx
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { LogBox, Platform } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import theme from "../../constants/theme";
 
+LogBox.ignoreLogs([
+  "Looks like you have configured linking in multiple places",
+]);
+
 export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Tabs
         screenOptions={{
           headerShown: false,
           tabBarActiveTintColor: theme.colors.accent,
           tabBarInactiveTintColor: theme.colors.text.primary,
+
+          //   paddingBottom: Platform.OS === "ios" ? 16 : 8,
+
           tabBarStyle: {
             backgroundColor: theme.colors.surface,
             borderTopColor: theme.colors.divider,
-            height: 64,
-            paddingTop: 2.5,
-            paddingBottom: Platform.OS === "ios" ? 16 : 8,
+
+            height:
+              Platform.OS === "android"
+                ? 50 + insets.bottom // 👈 fixes 3-button nav users
+                : 64,
+
+            paddingTop: 3,
+
+            paddingBottom:
+              Platform.OS === "android"
+                ? insets.bottom // 👈 pushes UI above nav buttons
+                : 16,
           },
         }}
       >
